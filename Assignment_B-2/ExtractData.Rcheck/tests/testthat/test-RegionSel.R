3ca7ebde5484840ab214a7bbcbbe2e0e152e57a1
Tzu-Yi Lu <ziyi.tzuyilu@gmail.com>
2021-11-16 13:59:44 -0800

    Updating test_that functions.

--- Assignment_B-2/ExtractData.Rcheck/tests/testthat/test-RegionSel.R
+++ Assignment_B-2/ExtractData.Rcheck/tests/testthat/test-RegionSel.R
@@ -1,3 +1,3 @@
 test_that("Check ouput class of the RegionSell function.", {
-  testthat::expect_true(is.data.frame(VanTreeUBC))
+  testthat::expect_true(is.data.frame(input_data))
 })
--- Assignment_B-2/ExtractData.Rcheck/tests/testthat/test-TreeAgeCal.R
+++ Assignment_B-2/ExtractData.Rcheck/tests/testthat/test-TreeAgeCal.R
@@ -1,4 +1,4 @@
 test_that("Check no NAs and column class in the TreeAgeCal function.", {
-  testthat::expect_equal(is.na(VanTreeUBC$tree_age), rep(FALSE,length(VanTreeUBC$tree_age)))
-  testthat::expect_true(is.numeric(VanTreeUBC$tree_age))
+  testthat::expect_equal(is.na(input_data$tree_age), rep(FALSE,length(input_data$tree_age)))
+  testthat::expect_true(is.numeric(input_data$tree_age))
 })
