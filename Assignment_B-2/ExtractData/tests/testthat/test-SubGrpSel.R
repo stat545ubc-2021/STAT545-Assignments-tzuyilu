45e1a74e1db9ff4748f9fb4df1b4dd34d483d517
Tzu-Yi Lu <ziyi.tzuyilu@gmail.com>
2021-11-20 15:39:13 -0800

    Updated function name.

--- Assignment_B-2/ExtractData/tests/testthat/test-SubGrpSel.R
+++ Assignment_B-2/ExtractData/tests/testthat/test-SubGrpSel.R
@@ -1,10 +1,10 @@
-test_that("RegionSel outputs data in a class of data frame", {
-  testthat::expect_true(is.data.frame(RegionSel(vancouver_trees,"KITSILANO")))
+test_that("SubGrpSel outputs data in a class of data frame", {
+  testthat::expect_true(is.data.frame(SubGrpSel(vancouver_trees,"KITSILANO")))
 })
 
 
-test_that("RegionSel chooses regions correctly", {
-  TR <- RegionSel(vancouver_trees,c("ARBUTUS-RIDGE","DUNBAR-SOUTHLANDS"))
+test_that("SubGrpSel chooses regions correctly", {
+  TR <- SubGrpSel(vancouver_trees,c("ARBUTUS-RIDGE","DUNBAR-SOUTHLANDS"))
   testthat::expect_equal(unique(TR$neighbourhood_name), c("ARBUTUS-RIDGE","DUNBAR-SOUTHLANDS"))
 })
 
