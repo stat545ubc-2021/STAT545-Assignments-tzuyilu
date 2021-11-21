1dedb4960cf14e713ef3cb3d2187da39b329d5ef
Tzu-Yi Lu <ziyi.tzuyilu@gmail.com>
2021-11-20 16:10:00 -0800

    Updated.

--- Assignment_B-2/ExtractData/R/TmeDifCal.R
+++ Assignment_B-2/ExtractData/R/TmeDifCal.R
@@ -8,6 +8,9 @@
 #' This parameter should be a character, which must be one of the columns of input_data.
 #' @param  end_date Either a specified date or another column as the end date.
 #' This parameter should be a character.
+#' @param  n_col_name The name for the new column you create in this function.
+#'
+#'
 #' @return  Input data with a column of time length of each observation.
 #'          The output will be a data frame.
 #'
@@ -15,22 +18,25 @@
 #'
 #' @export
 #' @examples
-#' TmeDifCal(vancouver_trees,"date_planted","2021-11-20")
+#' TmeDifCal(vancouver_trees,"date_planted","2021-11-20","tree_age")
 #'
 #'
 
-TmeDifCal <- function(input_data,start_date,end_date) {
+TmeDifCal <- function(input_data,start_date,end_date,n_col_name) {
   if (!is.data.frame(input_data)) {
     stop("The input data is not a data frame. The input data is ", class(input_data))
   }
 
   if (is.na(match(start_date,names(input_data)))) {
-    stop("Please specify one of the column as the start_date. There is no column named ",start_date,'.')
+    stop("Please check the [start_date] variable again. There is no column named ",start_date,'.')
   }
 
+  # Creating expression
+  exp_text <- paste('output_data <- input_data %>%
+    dplyr::mutate(',n_col_name,' = as.numeric(difftime(as.Date(start_date),end_date))/365) %>%
+    tidyr::drop_na()',sep='')
+  # Evaluating expression
+  eval(parse(text=exp_text))
 
-  output_data <- input_data %>%
-    dplyr::mutate(tree_age = as.numeric(difftime(as.Date(ref_date),date_planted))/365) %>%
-    tidyr::drop_na()
   return(output_data)
 }
