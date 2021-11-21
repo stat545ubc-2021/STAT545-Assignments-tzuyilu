375eadda18940a3073b37746e4550812faa3568f
Tzu-Yi Lu <ziyi.tzuyilu@gmail.com>
2021-11-20 16:08:24 -0800

    Modified content.

--- Assignment_B-2/ExtractData/R/TmeDifCal.R
+++ Assignment_B-2/ExtractData/R/TmeDifCal.R
@@ -1,28 +1,34 @@
-#' @title  Calculating the tree ages
+#' @title  Calculating time length
 #'
 #' @description   This function calculates the tree age of each observation in the input.
 #'
-#' @param  input_data Data with planted_date of trees. This parameter should be a data frame.
-#' @param  ref_date The Reference date for calculating the tree age of each observation. This parameter should be a string.
-#'
-#' @return  Input data with a column of the tree age of each observation.
+#' @param  input_data Data with planted_date of trees.
+#' This parameter should be a data frame.
+#' @param  start_date One of columns in a data set.
+#' This parameter should be a character, which must be one of the columns of input_data.
+#' @param  end_date Either a specified date or another column as the end date.
+#' This parameter should be a character.
+#' @return  Input data with a column of time length of each observation.
 #'          The output will be a data frame.
 #'
 #' @importFrom magrittr %>%
 #'
 #' @export
 #' @examples
-#' TreeAgeCal(datateachr::vancouver_trees,"2021-11-20")
-#' TreeAgeCal(datateachr::vancouver_trees,"2014-05-12")
+#' TmeDifCal(vancouver_trees,"date_planted","2021-11-20")
+#'
 #'
 
-TreeAgeCal <- function(input_data,ref_date) {
+TmeDifCal <- function(input_data,start_date,end_date) {
   if (!is.data.frame(input_data)) {
     stop("The input data is not a data frame. The input data is ", class(input_data))
   }
-  if (!is.character(ref_date)) {
-    stop("The ref_date is not a character. The ref_date is ", class(ref_date))
+
+  if (is.na(match(start_date,names(input_data)))) {
+    stop("Please specify one of the column as the start_date. There is no column named ",start_date,'.')
   }
+
+
   output_data <- input_data %>%
     dplyr::mutate(tree_age = as.numeric(difftime(as.Date(ref_date),date_planted))/365) %>%
     tidyr::drop_na()
